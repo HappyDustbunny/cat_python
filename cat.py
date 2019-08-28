262a6b1b87bb602a8020899897722aef84f14a49
Mads Horn <madshorn@operamail.com>
2019-08-28 20:07:01 +0200

    Added timestep and better drawing

--- cat.py
+++ cat.py
@@ -1,9 +1,11 @@
+
+
 from tkinter import Tk, Canvas, BOTH
 from tkinter.ttk import Frame
 
 # http://zetcode.com/tkinter/snake/
 
-class Cat(Frame):
+class Cat(Canvas):
 
     def __init__(self):
         super().__init__()
@@ -11,27 +13,25 @@ class Cat(Frame):
         self.pack()
 
     def initCat(self):
-        self.draw_buffer = []
+        self.draw_buffer = [(10,10,100,100), (100,100,100,200),(100,200,300,400)]
+        #self.draw_buffer = []
         self.inGame = True
         self.master.title("Cat")
         self.bind_all("<Key>", self.key_pressed)
         width = self.master.winfo_screenwidth()
         height = self.master.winfo_screenheight()
         self.master.geometry(f"{width}x{height}+{0}+{0}")
-        self.after(100, self.timer_event())
+        self.after(100, self.timer_event)
 
     def draw(self):
 
         canvas = Canvas(self)
+        canvas.delete("all")
 
         for line in self.draw_buffer:
             x1, y1, x2, y2 = line
             canvas.create_line(x1, y1, x2, y2)
-            print(line)
-
-        canvas.pack()
-
-        self.draw_buffer = []
+            canvas.pack()
 
     def key_pressed(self, e):
 
@@ -49,17 +49,20 @@ class Cat(Frame):
         if key == "Down":
             self.draw_buffer.append((50, 50, 50, 60))
             print("Down")
+        if key == "a":
+            self.draw_buffer = []
+            print("a")
 
     def timer_event(self):
         self.draw()
-        if self.inGame:
-            self.after(100, self.timer_event()) # The first number is time between updates in milliseconds.
+        #if self.inGame:
+        self.after(2000, self.timer_event) # The first number is time between updates in milliseconds.
 
 def main():
 
     root = Tk()
     root.wait_visibility(root)
-    root.wm_attributes('-alpha', 0.3)
+    root.wm_attributes('-alpha', 0.93)
     # root.geometry("500x500+300+800")
     # app = Example()
     app = Cat()
