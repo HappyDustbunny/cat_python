48ba5c27492d9c03d7e7d8a24c9a9f5e31b23c96
HappyDustbunny <madshorn@operamail.com>
2019-09-01 19:35:51 +0200

    Refactored and made pep8 compliant

--- cat.py
+++ cat.py
@@ -1,9 +1,10 @@
-from tkinter import Tk, Canvas, BOTH, Button
+from tkinter import Tk, Canvas, BOTH
 from tkinter.ttk import Frame
 
 # http://zetcode.com/tkinter/snake/
 # https://docs.python.org/3/library/tkinter.html
 # https://effbot.org/tkinterbook/canvas.htm
+
 
 class Canv(Canvas):
 
@@ -31,26 +32,32 @@ class Canv(Canvas):
         self.off_set_x = self.winfo_x()
         self.off_set_y = self.winfo_y()
         self.top_right = [self.width, 0]
-        self.top_left =  [0, 0]
+        self.top_left = [0, 0]
         self.bottom_left = [0, self.height]
         self.bottom_right = [self.width, self.height]
         self.diagonal_up = subtract(self.bottom_left, self.top_right)
         self.diagonal_down = subtract(self.top_left, self.bottom_right)
         self.reset()
         print("Canvas offset ", self.off_set_x, self.off_set_y)
-        print("Bzz", self.top_left, self.top_right, self.bottom_left, \
+        print("Bzz", self.top_left, self.top_right, self.bottom_left,
               self.bottom_right, self.diagonal_up, self.diagonal_down)
         self.after(100, self.timer_event)
 
     def draw(self):
 
+        self.draw_buffer = [self.top_pt, self.right_pt, self.bottom_pt,
+                            self.left_pt, self.top_pt, self.mid_top_right_pt,
+                            self.mid_bottom_left_pt, self.bottom_pt,
+                            self.mid_bottom_right_pt, self.mid_top_left_pt]
+
         for i, end in enumerate(self.draw_buffer):
             if i == 0:
                 start = end
                 continue
-            self.canvas.create_line(start[0], start[1], end[0], end[1], width=2, fill="red")
+            self.canvas.create_line(start[0], start[1], end[0], end[1],
+                                    width=2, fill="red")
             # self.canvas.create_line(start[0] - self.off_set_x, start[1] - \
-            #                         self.off_set_y, end[0] - self.off_set_x, \
+            #                         self.off_set_y, end[0] - self.off_set_x,
             #                         end[1] - self.off_set_y)
             self.canvas.pack(side='top', fill='both', expand=True)
             start = end
@@ -59,7 +66,8 @@ class Canv(Canvas):
 
         self.draw()
 
-        self.after(100, self.timer_event) # The first number is time between updates in milliseconds.
+        self.after(100, self.timer_event)
+        # The first number is time between updates in milliseconds.
 
     def key_pressed(self, e):
 
@@ -78,15 +86,18 @@ class Canv(Canvas):
             self.contract_to(self.mid_bottom_left_pt)
             print("Down")
         if key == "e":
-            self.event_generate('<Motion>', warp=True, x=self.center_pt[0] - \
-                                self.off_set_x, y=self.center_pt[1] - self.off_set_y)
+            self.event_generate('<Motion>', warp=True,
+                                x=self.center_pt[0] - self.off_set_x,
+                                y=self.center_pt[1] - self.off_set_y)
             print("e center", self.center_pt[0], self.center_pt[1])
-            print("e center - offset", self.center_pt[0] - self.off_set_x, self.center_pt[1] - self.off_set_y)
+            print("e center - offset", self.center_pt[0] - self.off_set_x,
+                  self.center_pt[1] - self.off_set_y)
             self.reset()
         if key == "a":
             xx = 1000
             yy = 500
-            self.event_generate('<Motion>', warp=True, x=xx - self.off_set_x, y=yy - self.off_set_y)
+            self.event_generate('<Motion>', warp=True, x=xx - self.off_set_x,
+                                y=yy - self.off_set_y)
             print("a", self.winfo_pointerx(), self.winfo_pointery())
         if key == "b":
             self.event_generate('<Motion>', warp=True, x=-450, y=-757)
@@ -114,10 +125,10 @@ class Canv(Canvas):
         self.center_pt = add(self.bottom_left, times_scalar(-0.5, self.diagonal_up))
         self.zoom = 0
 
-        self.draw_buffer = [self.top_pt, self.right_pt, self.bottom_pt, self.left_pt, \
-                            self.top_pt, self.mid_top_right_pt, \
-                            self.mid_bottom_left_pt, self.bottom_pt, \
-                            self.mid_bottom_right_pt, self.mid_top_left_pt]
+        # self.draw_buffer = [self.top_pt, self.right_pt, self.bottom_pt, self.left_pt, \
+        #                     self.top_pt, self.mid_top_right_pt, \
+        #                     self.mid_bottom_left_pt, self.bottom_pt, \
+        #                     self.mid_bottom_right_pt, self.mid_top_left_pt]
         # print("Resat", self.draw_buffer)
 
     def contract_to(self, left_point):
@@ -136,28 +147,32 @@ class Canv(Canvas):
         self.mid_bottom_left_pt =  add(left_point, times_scalar(0.5, subtract(self.new_bottom, left_point)))
         self.center_pt = add(left_point, times_scalar(0.5, subtract(self.new_right, left_point)))
         self.zoom += 1
-
-        self.draw_buffer = [self.top_pt, self.right_pt, self.bottom_pt, self.left_pt, \
-                            self.top_pt, self.mid_top_right_pt, \
-                            self.mid_bottom_left_pt, self.bottom_pt, \
-                            self.mid_bottom_right_pt, self.mid_top_left_pt]
+        #
+        # self.draw_buffer = [self.top_pt, self.right_pt, self.bottom_pt, self.left_pt, \
+        #                     self.top_pt, self.mid_top_right_pt, \
+        #                     self.mid_bottom_left_pt, self.bottom_pt, \
+        #                     self.mid_bottom_right_pt, self.mid_top_left_pt]
 
         # print("Contracted to", self.draw_buffer)
 
         if self.zoom > 10:
             self.reset()
 
+
 def add(a, b):
     # Vector addition
     return [a[0] + b[0], a[1] + b[1]]
+
 
 def subtract(a, b):
     # Vector subtraction
     return [a[0] - b[0], a[1] - b[1]]
 
+
 def times_scalar(t, a):
     # Scalar times Vector
     return [t * a[0], t * a[1]]
+
 
 def main():
 
@@ -167,7 +182,7 @@ def main():
     root.wm_attributes('-fullscreen', 1)
     # root.geometry("500x500+300+800")
     # app = Example()
-    app = Canv()
+    # app = Canv()
 
     root.mainloop()
 
