f489eea77f901e09617fac35a1eb9f1d61613a1c
Mads Horn <madshorn@operamail.com>
2019-09-07 13:03:20 +0200

    Line thickness diminishes as rhombe shrink

---
+++ cat.bat
@@ -0,0 +1,3 @@
+@ECHO off
+SET MY_PATH=C:\Users\Mads Horn\python_projects\cat_python\dist\cat
+@START "%MY_PATH%\cat.exe
--- cat.py
+++ cat.py
@@ -66,7 +66,7 @@ class Canv(Canvas):
                 start = end
                 continue
             self.canvas.create_line(start[0], start[1], end[0], end[1],
-                                    width=3, fill="blue2")
+                                    width=4 - self.zoom/2, fill="blue2")
 
             self.canvas.pack(side='top', fill='both', expand=True)
             start = end
